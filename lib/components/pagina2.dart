810da67103a88772e14d0f4824409c77443ff898
joel <joelramos@ceroca.cat>
2024-12-03 12:25:39 +0100

    Comentarios

--- lib/components/pagina2.dart
+++ lib/components/pagina2.dart
@@ -8,6 +8,23 @@ class Pagina2 extends StatelessWidget {
       appBar: Navegacio(title: "Star Trips - Configura el viatge", backcolor: Color.fromARGB(255, 1, 10, 90), colorText: Color.fromARGB(255, 1, 253, 241)),
       body: Center(
         child: Text("Pagina 2"),
+
+        //Titol pasado desde widget (Passatger/a)
+        //TextInput con el texto de dentro pasado por widget
+        
+
+        //Titol pasado desde widget (Escull les caractersitiques del viatge)
+        //Checkbox + Anada i Tornada
+
+        //ROW CON - (Origen) + debajo (una lista con diferentes opciones) - (Destinacio) + debajo (una lista con diferentes opciones)
+
+        //Titol pasado desde widget (Com vols navegar?)
+        //3 checkbox 1. (1a classe) 2.(Bussines) 3.(Economy)
+
+        //Titol pasado desde widget (Quin tipus de viatge vols fer?)
+        // Checkbox (1. Negocis 2. Vacances)
+
+        //Boton de enviar (Enviar dades)
       ),
     );
   }
